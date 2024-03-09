46232aef0303de6d26a286fcd1c4c9d7132fed8e
TP2512 <tarkesh2512@gmail.com>
2024-03-09 20:56:35 +0530

    added SA app and fastapi get sent against news and webscraper done

--- News_Aggregator/Web_Scrapper/news_scrapper_app.py
+++ News_Aggregator/Web_Scrapper/news_scrapper_app.py
@@ -4,15 +4,21 @@ from bs4 import BeautifulSoup
 from News_Aggregator.Web_Scrapper import news_scrapper_main as nws
 import time
 import asyncio
-from Web_Scrapper.Database import mongodb_connection as mc
+from Database import mongodb_connection as mc
 from datetime import datetime
+from services import get_sentiment as sg
 
 db_handler = mc.MongoDBConnection("news_aggregator_db", "news_articles")
 
 
 def push_to_mongodb(data):
     data.update({"created_date": datetime.now(), "user": "webscraper"})
-    # data.update({"sentiment": get_sentiment(data["content"])})
+    text = data["content"]
+    escaped_news = text.replace('"', '\\"')
+    senti_getter = sg.SentimentAnalysis(escaped_news)
+    sentiment = senti_getter.get_sentiment_from_app()
+    data.update({"sentiment": sentiment})
+    # print(data["sentiment"])
     result = db_handler.collection.insert_one(data)
     print(f"Inserted news article with ID: {result.inserted_id}")
 
@@ -21,10 +27,10 @@ async def scrape_and_push(url, parser=None):
     try:
         scrapper = nws.WebScrapper(url, parser)
         news_articles = await scrapper.scrape()
-        print(url.split('.')[0].split('/')[-1], ":")
+        # source = url.split('.')[0].split('/')[-1], ":"
         for article in news_articles:
-            # push_to_mongodb(article)
-            print(article)
+            push_to_mongodb(article)
+            # print(article)
     except TypeError:
         print(f"Internet connection is broken or url dont have any relative content {url}")
         logger.error(f"Internet connection is broken or url dont have any relative content {url}")
