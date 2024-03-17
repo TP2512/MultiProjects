3b158733ee9ac85e7086d4d16256f1903bb27155
TP2512 <tarkesh2512@gmail.com>
2024-03-17 11:33:11 +0530

    adding coomit for deployment

--- News_Aggregator/Sentiment_Analysis/SentimentAna_App/sentimentanalysis.py
+++ News_Aggregator/Sentiment_Analysis/SentimentAna_App/sentimentanalysis.py
@@ -2,7 +2,10 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from typing import Text
 from service import setment_responder as sr
+from loggers import configure_logger
 
+
+logger = configure_logger()
 app = FastAPI()
 
 
@@ -13,4 +16,5 @@ class NewsInput(BaseModel):
 @app.post("/get_sentiment")
 async def get_sentiment(news:  NewsInput):
     sg = sr.NewsAnalysis(news.news_article)
+    logger.info("got request for sentiment from news")
     return sg.sentiment_getter()
